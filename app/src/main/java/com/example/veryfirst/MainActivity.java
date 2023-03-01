9d876981f4a80cd6c6367072cde328fbe7820518
TeoMilligan <Olipino@internet.ru>
2023-03-01 22:46:03 +0300

    Have learned all View and started layouts

--- app/src/main/java/com/example/veryfirst/MainActivity.java
+++ app/src/main/java/com/example/veryfirst/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.veryfirst;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+
+       TextView textView = (TextView) findViewById(R.id.normal);
+       textView.setText("Text from java code");
+       textView.setTextColor(Color.RED);
 
     /*   TextView textView = (TextView)  findViewById(R.id.header);
 
